faca4e21acc625288f9bdf192b14504b6869b741
mendozalz <mendoza124302@gmail.com>
2025-02-23 23:02:11 -0500

    Excersice Complete

---
+++ LogicCourse/ParImpar/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString= string.Empty;
+do
+{
+    Console.Write("Ingresa un número entero o 'S' para salir: ");
+    numberString = Console.ReadLine()!.ToLower();
+    var numberInt = 0;
+    if(numberString == "s")
+    {
+        break;
+    }
+
+    if(int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt} es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{numberString} no es un número entero valido");
+    }
+} while (numberString != "s");
+Console.WriteLine("Fin del programa");
